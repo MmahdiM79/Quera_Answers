ae607b3d0536668fb770eccc61867790ccf573c5
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-10-15 19:59:22 +0330

    bugs fixed

--- Q3539_(تک رقمی)/main.py
+++ Q3539_(تک رقمی)/main.py
@@ -1,11 +1,24 @@
 
 
+
+def case0(num: int):
+    if n%9 == 0:
+        print(9)
+        exit()
+    
+
+
+
 if __name__ == "__main__":
     
     n = int(input())
     
+    case0(n)
+    
     while n > 10:
         n %= 9
+        case0(n)
+    
         
     print(n)
     
