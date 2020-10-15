025b9aeea2caeb2c787b2cd35097f3c3a043ed59
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-10-15 19:40:39 +0330

    answer of Q2885 in Java added

---
+++ Q2885_(یک سوال ساده)/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        int n = (new Scanner(System.in)).nextInt();
+
+        do
+        {
+            System.out.println("man khoshghlab hastam");
+        } while (--n > 0);
+    }
+}
