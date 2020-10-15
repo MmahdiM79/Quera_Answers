01e470850be1baf9ed186e1fd8abee796b94d6eb
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-10-15 20:01:10 +0330

    bugs fixed

--- Q3539_(تک رقمی)/main.py
+++ Q3539_(تک رقمی)/main.py
@@ -1,10 +1,7 @@
 
 
 
-def case0(num: int):
-    if n%9 == 0:
-        print(9)
-        exit()
+
     
 
 
@@ -13,12 +10,11 @@ if __name__ == "__main__":
     
     n = int(input())
     
-    case0(n)
-    
+
     while n > 10:
         n %= 9
-        case0(n)
+        
     
         
-    print(n)
+    print(n) if n !=0 else print(9)
     
