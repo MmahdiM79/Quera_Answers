fb8e340f90301513456a7557b474f4604890a9d9
MmahdiM79 <MmahdiM79@users.noreply.github.com>
2020-10-25 11:39:37 +0330

    minor changes

--- Q10230_(مشق امشب باقر)/main.py
+++ Q10230_(مشق امشب باقر)/main.py
@@ -6,13 +6,13 @@
 if __name__ == "__main__":
     
     
-    degrees = [int(degree) for degree in input().split()]
+    angles = [int(angle) for angle in input().split()]
     
-    if any((degree == 0) for degree in degrees):
+    if any((angle == 0) for angle in angles):
         print("No")
         exit()
         
-    if sum(degrees) == 180:
+    if sum(angles) == 180:
         print("Yes")
     else:
         print("No")
